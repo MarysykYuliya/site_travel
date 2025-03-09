9d8e815af960daff1461e6972e85dd702dda7c42
MarysykYuliya <marissik.yuliya@gmal.com>
2025-03-09 19:01:38 +0200

    Ready lr

---
+++ folder-page/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const thumbnails = document.querySelectorAll(".thumbnail");
+  const modal = document.getElementById("modal");
+  const fullImage = document.getElementById("fullImage");
+  const closeBtn = document.querySelector(".close");
+
+  thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener("click", function () {
+      modal.style.display = "flex";
+      fullImage.src = this.src;
+    });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  // Закриття модального вікна при кліку за його межами
+  modal.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
